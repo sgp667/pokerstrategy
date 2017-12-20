47749d75444eff773864afcb53592a38f11a1ca0
EC2 Default User <ec2-user@ip-172-31-69-26.ec2.internal>
2017-12-20 03:40:22 +0000

    Added a test suite

--- pokerstrategy/deck.py
+++ pokerstrategy/deck.py
@@ -1,4 +1,10 @@
 
 class CardDeck():
-    def __init__(self,decks):
-    
+    suites = ['Clubs','Diamonds','Hearts','Spades']
+    ranks = range(0,13)
+     
+    def __init__(self,decks = 1):
+        single_deck = [(suite, rank) for suite in suites for rank in ranks]
+        self.cards  = [single_deck for x in range(0,decks)]
+     
+     
---
+++ tests/test_deck.py
@@ -0,0 +1,9 @@
+import pytest
+from pokerstrategy import CardDeck
+
+def test_dummy_Success():
+    assert True
+    
+def test_CardDeck_exists():
+    deck = CardDeck()
+
