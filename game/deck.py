a8038ac4697ab4f26f35376b0385973846e6cb10
EC2 Default User <ec2-user@ip-172-31-69-26.ec2.internal>
2017-12-21 12:56:52 +0000

    Extpanded test to make sure there is always the right amount of cards in teh game

--- game/deck.py
+++ game/deck.py
@@ -4,5 +4,12 @@ ranks = range(0,13)
 class CardDeck:
     def __init__(self,decks = 1):
         self.cards  = [(suite, rank) for suite in suites for rank in ranks for x in range(0,decks)]
-     
+        self.count  = len(self.cards)
+        
+    def draw(self):
+        deck = self.cards
+        drawn_card = deck.pop
+        self.cards = deck
+        self.count = len(deck)
+        return drawn_card
      
--- tests/test_deck.py
+++ tests/test_deck.py
@@ -5,10 +5,34 @@ def test_CardDeck_exists():
     cardeck = deck.CardDeck()
     assert type(cardeck) == deck.CardDeck
 
+
+# Test that decks always have teh right amount of cards even after you draw a card
 def test_singe_CardDeck_has_52_cards():
     cardeck = deck.CardDeck()
     assert len(cardeck.cards) == 52
+    assert cardeck.count      == 52
 
 def test_two_CardDeck_has_x_cards():
     cardeck = deck.CardDeck(2)
     assert len(cardeck.cards) == 104
+    assert cardeck.count      == 104
+    
+
+@pytest.mark.parametrize("init_decks,draws,expected",[
+    (1,0,52),
+    (2,0,104),
+    (1,1,51),
+    (1,2,50)])
+def test_CardDeck_has_x_cards(init_decks,draws,expected):
+    cardeck = deck.CardDeck(init_decks)
+    drawncards = list()
+    
+    for x in range(draws):
+        card = cardeck.draw()
+        drawncards.append(card)
+        
+    assert len(cardeck.cards) == expected
+    assert cardeck.count      == expected
+    assert len(drawncards)    == draws
+    
+# TODO Write a test case where CardDeck trows an error if you try to draw a card after all cards have been drawn
